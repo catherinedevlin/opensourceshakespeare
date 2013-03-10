94e40f146c96f7f22923bad04af4f1f674b54647
Catherine Devlin <catherine_devlin@kace.com>
2013-03-09 21:37:08 -0500

    characters, chapters, paragraphs added

--- schema_pg.sql
+++ schema_pg.sql
@@ -1,17 +1,78 @@
-DROP TABLE IF EXISTS works_raw CASCADE;
-CREATE TABLE works_raw
+DROP TABLE IF EXISTS work CASCADE;
+CREATE TABLE work
   ( workID    VARCHAR(32) PRIMARY KEY,
-    title     VARCHAR(200) NOT NULL,
-    longTitle VARCHAR(1000),
-    year      INTEGER,
-    genreType VARCHAR(10),
+    title     VARCHAR(32) NOT NULL,
+    longTitle VARCHAR(64) NOT NULL,
+    year      INTEGER NOT NULL,
+    genreType VARCHAR(1) NOT NULL,
     notes     TEXT,
-    source    VARCHAR(100),
-    totalWords      INTEGER,
-    totalParagraphs INTEGER
+    source    VARCHAR(16) NOT NULL,
+    totalWords      INTEGER NOT NULL,
+    totalParagraphs INTEGER NOT NULL
   );
 
-COPY works_raw FROM '/tmp/opensourceshakespeare/Works.txt'
+COPY work FROM '/tmp/opensourceshakespeare/Works.txt'
+  WITH (FORMAT csv,
+        QUOTE '~');
+
+DROP TABLE IF EXISTS character CASCADE;
+CREATE TABLE character 
+  ( charID      VARCHAR(32) PRIMARY KEY,
+    charName    VARCHAR(64) NOT NULL,
+    abbrev      VARCHAR(32),
+    works       VARCHAR(256) NOT NULL,
+    description VARCHAR(2056),
+    speechCount INTEGER NOT NULL
+  );
+        
+COPY character FROM '/tmp/opensourceshakespeare/Characters.txt'
+  WITH (FORMAT csv,
+        QUOTE '~');
+
+DROP TABLE IF EXISTS character_work;
+CREATE TABLE character_work
+  ( charID     VARCHAR(32) NOT NULL REFERENCES character (charID),
+    workID     VARCHAR(32) NOT NULL REFERENCES work (workID),
+    PRIMARY KEY (charID, workID)
+  );
+
+INSERT INTO character_work (charID, workID)
+SELECT charID,
+       regexp_split_to_table(works, ',')
+FROM   character;
+
+ALTER TABLE character DROP COLUMN works;
+
+DROP TABLE IF EXISTS chapter;
+CREATE TABLE chapter 
+  ( workID         VARCHAR(32) NOT NULL REFERENCES work (workID),
+    chapterID      INTEGER PRIMARY KEY,
+    section        INTEGER NOT NULL,
+    chapter        INTEGER NOT NULL,
+    description    VARCHAR(256) NOT NULL
+  );
+
+COPY chapter FROM '/tmp/opensourceshakespeare/Chapters.txt'
+  WITH (FORMAT csv,
+        QUOTE '~');
+
+DROP TABLE IF EXISTS paragraph;
+CREATE TABLE paragraph
+  ( workID         VARCHAR(32) NOT NULL REFERENCES work (workID),
+    paragraphID    INTEGER PRIMARY KEY NOT NULL,
+    paragraphNum   INTEGER NOT NULL,
+    charID         VARCHAR(32) NOT NULL REFERENCES character (charID),
+    plainText      TEXT NOT NULL,
+    phoneticText   TEXT NOT NULL,
+    stemText       TEXT NOT NULL,
+    paragraphType  VARCHAR(1) NOT NULL,
+    section        INTEGER NOT NULL,
+    chapter        INTEGER NOT NULL,
+    charCount      INTEGER NOT NULL,
+    wordCount      INTEGER NOT NULL
+  );
+
+COPY paragraph FROM '/tmp/opensourceshakespeare/Paragraphs.txt'
   WITH (FORMAT csv,
         QUOTE '~');
 
